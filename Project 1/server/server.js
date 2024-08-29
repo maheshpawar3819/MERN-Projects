74f20cf8189902950c914e90a992a002e0b12f89
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-29 16:52:09 +0530

    add another route contact create model and add data successfully in the database

--- Project 1/server/server.js
+++ Project 1/server/server.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const router = require("./router/auth-router");
+const authRoute = require("./router/auth-router");
+const contactRoute = require("./router/contact-router");
 const connectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 const port = 8080;
@@ -9,7 +10,8 @@ const port = 8080;
 //middleware
 //for using json in the application
 app.use(express.json());
-app.use("/api/auth", router);
+app.use("/api/auth", authRoute);
+app.use("/api/form", contactRoute);
 app.use(errorMiddleware);
 
 //logic start server only when the db connect succssfully
