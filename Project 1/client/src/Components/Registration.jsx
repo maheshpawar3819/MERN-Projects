f25817e50c436c9c1e076d4d56f5ee3c00e9bab1
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-08-30 14:53:33 +0530

    create registration form

--- Project 1/client/src/Components/Registration.jsx
+++ Project 1/client/src/Components/Registration.jsx
@@ -1,11 +1,112 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Registration = () => {
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    password: "",
+  });
+
+  const handleInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    setUser({
+      ...user,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault;
+  };
+
   return (
     <div>
-      This is an registration page
-    </div>
-  )
-}
+      <section>
+        <main>
+          <div className="section-registration">
+            <div className="container grid grid-two-cols">
+              <div className="registration-image">
+                <img
+                  src="/images/register.png"
+                  alt="the girl trying to fill registeration form"
+                  width={"500"}
+                  height={"500"}
+                />
+              </div>
 
-export default Registration
+              {/* registeration form  */}
+
+              <div className="registration-form">
+                <h1 className="main-heading mb-3">registration form</h1>
+                <br />
+                <form action="" onSubmit={handleSubmit}>
+                  <div>
+                    <label htmlFor="usesrname">usesrname</label>
+                    <input
+                      type="text"
+                      name="username"
+                      placeholder="Enter your username"
+                      id="username"
+                      required
+                      autoComplete="off"
+                      value={user.username}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="email">email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Enter your email"
+                      id="email"
+                      required
+                      autoComplete="off"
+                      value={user.email}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="phone">phone</label>
+                    <input
+                      type="number"
+                      name="phone"
+                      placeholder="Enter your phone"
+                      id="phone"
+                      required
+                      autoComplete="off"
+                      value={user.phone}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <div>
+                    <label htmlFor="password">password</label>
+                    <input
+                      type="password"
+                      name="password"
+                      placeholder="Enter your password"
+                      id="password"
+                      required
+                      autoComplete="off"
+                      value={user.password}
+                      onChange={handleInput}
+                    />
+                  </div>
+                  <br />
+                  <button type="submit" className="btn btn-submit">
+                    Register Now
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </main>
+      </section>
+    </div>
+  );
+};
+
+export default Registration;
