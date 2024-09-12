a05b0494157af8ebafb932918d809f7a69d78c09
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-09-12 16:55:29 +0530

    update server

--- Inventory/Server/server.js
+++ Inventory/Server/server.js
@@ -1,12 +1,25 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const app = express();
-const port = 8080;
-const cors=require("cors");
+const cors = require("cors");
+const connectDb = require("./config/db");
+const authRoutes = require("./routes/authRouters");
+const porductRouters = require("./routes/productRouters");
+
+dotenv.config();
 
 //middlewares
 app.use(cors());
 app.use(express.json());
 
+//path
+app.use("/api/auth", authRoutes);
+app.use("/api/products", porductRouters);
+
+//define port
+const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
+  connectDb();
   console.log(`server was listning on port ${port}`);
 });
