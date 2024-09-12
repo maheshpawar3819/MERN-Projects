429cf7e76e6928224fcbb75d52f22bdee2767920
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-09-12 16:28:18 +0530

    add middleware to check token

---
+++ Inventory/Server/middleware/authMiddleware.js
@@ -0,0 +1,25 @@
+const jwt=require("jsonwebtoken");
+const User=require("../models/userModel");
+
+const protect=async (req,res,next) => {
+    let token;
+
+    if(req.headers.authorization && req.headers.authorization.startWith('Bearer')){
+        //extarct the token in the header 
+        token=req.headers.authorization.split(" ")[1];
+    }
+
+    if(!token){
+        return res.status(401).json({message:"not authorized,token not valid"});
+    }
+
+    try {
+        const decoded=jwt.verify(token,process.env.JWT_SECRET_KEY);
+        //exclude the passowrd
+        req.user=await User.findById(decoded.id).select("-password");
+    } catch (error) {
+        next(error);
+    }
+}
+
+module.exports(protect);
