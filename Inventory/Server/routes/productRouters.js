84fd51a935324b7e7eb847e5b61bb67e59b0cc22
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-09-12 16:46:53 +0530

    add routes for product

---
+++ Inventory/Server/routes/productRouters.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const { protect } = require("../middleware/authMiddleware");
+const {
+  getProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/productController");
+
+//define routes
+router.route("/").get(protect, getProducts).post(protect, createProduct);
+router.route("/:id").put(protect, updateProduct).delete(protect, deleteProduct);
