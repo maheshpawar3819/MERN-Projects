47eec359b0f37819e76a13675543c74d08a9d169
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-09-19 20:02:59 +0530

    add react for frontend

---
+++ CRUD2 (with prisma)/Client/CrudWithPrisma/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='bg-gray-400 text-center text-6xl text-amber-800'>hi mahi</h1>
+    </>
+  )
+}
+
+export default App
