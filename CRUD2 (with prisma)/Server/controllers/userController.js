11fe814bd7a1e28fa1c502564ea144584466e660
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-09-19 19:46:38 +0530

    add route to delete user

--- CRUD2 (with prisma)/Server/controllers/userController.js
+++ CRUD2 (with prisma)/Server/controllers/userController.js
@@ -1,72 +1,90 @@
-const {PrismaClient}=require("@prisma/client")
-const prisma=new PrismaClient();
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-const home=async (req,res) => {
-    try {
-        const users=await prisma.user.findMany()
-        res.status(200).json(users);
-    } catch (error) {
-        res.status(500).json({msg:"something wrong internal server error"});
-    }
-}
+const home = async (req, res) => {
+  try {
+    const users = await prisma.user.findMany();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ msg: "something wrong internal server error" });
+  }
+};
 
 //create user
 
-const createUser=async (req,res) => {
-    try {
-        const {name,email,password}=req.body;
+const createUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-        const exist=await prisma.user.findUnique({
-            where:{email:email}
-        });
-        if(exist){
-            res.status(400).json({msg:`user is already exist`});
-        }
-
-        const newUser=await prisma.user.create({
-            data:{name,email,password}
-        });
-        
-        res.status(200).json({msg:'user create succesfully',user:newUser});
-    } catch (error) {
-        console.log(`someting wrong not able to create user`);
+    const exist = await prisma.user.findUnique({
+      where: { email: email },
+    });
+    if (exist) {
+      res.status(400).json({ msg: `user is already exist` });
     }
-}
+
+    const newUser = await prisma.user.create({
+      data: { name, email, password },
+    });
+
+    res.status(200).json({ msg: "user create succesfully", user: newUser });
+  } catch (error) {
+    console.log(`someting wrong not able to create user`);
+  }
+};
 
 //read user by id
 
-const getUser=async (req,res) => {
-    try {
-        const {id}=req.params;
-        const user=await prisma.user.findUnique({
-            where:{id:parseInt(id)}
-        })
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).json({msg:"someting wrong cannot able to find user by id"});
-    }
-}
+const getUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await prisma.user.findUnique({
+      where: { id: parseInt(id) },
+    });
+    res.status(200).json(user);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ msg: "someting wrong cannot able to find user by id" });
+  }
+};
 
 //update user
 
-const updateUser=async (req,res)=> {
-    try {
-        const {id}=req.params;
-        const {name,email}=req.body;
-        const updateuser=await prisma.user.update({
-            where:{id:parseInt(id)},
-            data:{name,email},
-            select:{
-                id:true,
-                name:true,
-                email:true,
-                password:false
-            }
-        })
-        res.status(200).json({msg:`user update successfully`,updateduser:updateuser});
-    } catch (error) {
-        res.status(500).json({msg:`someting wrong unable to update user`});
-    }
-}
+const updateUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email } = req.body;
+    const updateuser = await prisma.user.update({
+      where: { id: parseInt(id) },
+      data: { name, email },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        password: false,
+      },
+    });
+    res
+      .status(200)
+      .json({ msg: `user update successfully`, updateduser: updateuser });
+  } catch (error) {
+    res.status(500).json({ msg: `someting wrong unable to update user` });
+  }
+};
 
-module.exports={home,createUser,getUser,updateUser}
+//delete user
+
+const deletUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const removeUser = await prisma.user.delete({
+      where: { id: parseInt(id) },
+    });
+    res.status(200).json({ msg: `user delete user successfully`,user:removeUser });
+  } catch (error) {
+    res.status(500).json({ msg: `someting wrong not able to delete user` });
+  }
+};
+
+module.exports = { home, createUser, getUser, updateUser, deletUser };
--- CRUD2 (with prisma)/Server/route/userRoute.js
+++ CRUD2 (with prisma)/Server/route/userRoute.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const { home, createUser, getUser,updateUser } = require("../controllers/userController");
+const { home, createUser, getUser,updateUser ,deletUser} = require("../controllers/userController");
 
 //home route
 router.route("/").get(home);
 router.route("/createUser").post(createUser);
 router.route("/user/:id").get(getUser);
 router.route("/user/:id").put(updateUser);
+router.route("/user/:id").delete(deletUser);
 
 module.exports = router;
