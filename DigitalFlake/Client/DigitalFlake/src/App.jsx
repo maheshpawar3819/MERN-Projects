4ea88eb04053a3362bf8c26ce5985f8113a81da9
Mahesh Pawar <maheshpawr3819@gmail.com>
2024-10-01 10:56:34 +0530

    add react for frontend

---
+++ DigitalFlake/Client/DigitalFlake/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="bg-gray-500">hii</h1>
+    </>
+  );
+}
+
+export default App;
