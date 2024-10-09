bc9c641dcb0716e53a1ce5f20750939995bbdd43
maheshpawar3819 <pawarmahesh3819@gmail.com>
2024-10-09 12:34:31 +0530

    add styling to edit category component

--- DigitalFlake/Client/DigitalFlake/src/Components/Category.jsx/EditCategory.jsx
+++ DigitalFlake/Client/DigitalFlake/src/Components/Category.jsx/EditCategory.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 const EditCategory = () => {
   const [data, setData] = useState({
@@ -66,34 +67,62 @@ const EditCategory = () => {
   };
 
   return (
-    <div>
-      <form action="" onSubmit={updateCategory}>
-        <label htmlFor="newCategory">Category name</label>
-        <input
-          type="text"
-          name="name"
-          placeholder="enter category name"
-          value={data.name}
-          onChange={handleChange}
-        />
-        <label htmlFor="imgage">Peste imageUrl</label>
-        <input
-          type="text"
-          name="imageUrl"
-          placeholder="put your image url"
-          value={data.imageUrl}
-          onChange={handleChange}
-        />
-        <label htmlFor="status">Status</label>
-        <input
-          type="text"
-          name="status"
-          placeholder="give status"
-          value={data.status}
-          onChange={handleChange}
-        />
-        <button className="bg-green-600 p-1 rounded-md">Update</button>
-      </form>
+    <div className="ml-72 mt-20 p-4 shadow-md rounded-md m-2 h-[85vh]">
+      <div className="flex items-center">
+        <Link to={"/category"}>
+          <IoMdArrowRoundBack className="size-8" />
+        </Link>
+        <p className="text-2xl ml-5 font-semibold">Add Catogory</p>
+      </div>
+      <div>
+        <form action="" onSubmit={updateCategory}>
+          <label
+            htmlFor="newCategory"
+            className="text-xl font-semibold text-gray-700 mb-2"
+          >
+            Category name
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="enter category name"
+            value={data.name}
+            onChange={handleChange}
+            className="w-full px-4 mt-3 mb-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
+          />
+          <label
+            htmlFor="imgage"
+            className="text-xl font-semibold text-gray-700 mb-2"
+          >
+            Peste imageUrl
+          </label>
+          <input
+            type="text"
+            name="imageUrl"
+            placeholder="put your image url"
+            value={data.imageUrl}
+            onChange={handleChange}
+            className="w-full px-4 mt-3 mb-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
+          />
+          <label
+            htmlFor="status"
+            className="text-xl font-semibold text-gray-700 mb-2"
+          >
+            Status
+          </label>
+          <input
+            type="text"
+            name="status"
+            placeholder="give status"
+            value={data.status}
+            onChange={handleChange}
+            className="w-full px-4 mt-3 mb-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
+          />
+          <button className="bg-[#662671] hover:bg-[#823c8f] text-white px-3 tracking-wider p-2 rounded-md">
+            Update
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
