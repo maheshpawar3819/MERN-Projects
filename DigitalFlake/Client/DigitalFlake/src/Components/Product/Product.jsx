70a073be667c517ea9e97f3f6f576d6ee71b76b2
maheshpawar3819 <pawarmahesh3819@gmail.com>
2024-10-09 14:24:19 +0530

    add style to product component

--- DigitalFlake/Client/DigitalFlake/src/Components/Product/Product.jsx
+++ DigitalFlake/Client/DigitalFlake/src/Components/Product/Product.jsx
@@ -38,20 +38,33 @@ const Product = () => {
   };
 
   return (
-    <div>
-      <button className="bg-slate-500 p-1 rounded-md px-5">
-        <Link to={"/product/add"}>Add</Link>
-      </button>
-      <table border="1" cellPadding="10" cellSpacing="0">
-        <thead>
+    <div className="ml-72 mt-20 p-4">
+      <div className="flex justify-between items-center mb-4">
+        <button className="bg-[#662671] hover:bg-[#823c8f] text-white py-2 px-5 rounded-md">
+          <Link to={"/product/add"}>Add New</Link>
+        </button>
+        <p className="text-3xl font-bold font-mono">Product</p>
+        <input
+          type="text"
+          placeholder="Search category..."
+          className="p-2 border rounded-md w-1/3"
+        />
+      </div>
+      <table
+        border="1"
+        cellPadding="10"
+        cellSpacing="0"
+        className="w-full bg-white shadow-md rounded-lg overflow-hidden"
+      >
+        <thead className="bg-yellow-200 text-left">
           <tr>
-            <th>ID</th>
-            <th>Product Name</th>
-            <th>Image</th>
-            <th>Status</th>
-            <th>SubCategory</th>
-            <th>Category</th>
-            <th>Action</th>
+            <th className="p-3">ID</th>
+            <th className="p-3">Product Name</th>
+            <th className="p-3">Image</th>
+            <th className="p-3">Status</th>
+            <th className="p-3">SubCategory</th>
+            <th className="p-3">Category</th>
+            <th className="p-3">Action</th>
           </tr>
         </thead>
         <tbody>
@@ -59,9 +72,9 @@ const Product = () => {
             data.map((ele) => {
               const { id, imageUrl, name, status } = ele;
               return (
-                <tr key={id}>
-                  <td>{id}</td>
-                  <td>{name}</td>
+                <tr key={id} className="border-b hover:bg-gray-100">
+                  <td className="p-3">{id}</td>
+                  <td className="p-3">{name}</td>
                   <td>
                     <img
                       src={imageUrl}
@@ -71,15 +84,21 @@ const Product = () => {
                       className="rounded-lg"
                     />
                   </td>
-                  <td>{status}</td>
-                  <td>{ele?.subcategory?.name}</td>
-                  <td>{ele?.category?.name}</td>
-                  <td>
-                    <button className="p-1 bg-green-500 px-2 rounded-md text-white">
+                  <td
+                    className={`p-3 ${
+                      status === "active" ? "text-green-600" : "text-red-600"
+                    }`}
+                  >
+                    {status}
+                  </td>
+                  <td className="p-3">{ele?.subcategory?.name}</td>
+                  <td className="p-3">{ele?.category?.name}</td>
+                  <td className="p-3 flex space-x-2 mt-2">
+                    <button className="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
                       <Link to={`/product/${id}`}> Edit</Link>
                     </button>
                     <button
-                      className="p-1 bg-red-500 px-2 rounded-md text-white"
+                      className="p-2 bg-red-500 text-white rounded-md hover:bg-red-600"
                       onClick={() => deleteProduct(id)}
                     >
                       Delete
