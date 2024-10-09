822a725d4c46f24e3c397badff1fbaedf2bd3725
maheshpawar3819 <pawarmahesh3819@gmail.com>
2024-10-09 12:57:21 +0530

    add styling to addsubcategory component

--- DigitalFlake/Client/DigitalFlake/src/Components/SubCategory/AddSubCategory.jsx
+++ DigitalFlake/Client/DigitalFlake/src/Components/SubCategory/AddSubCategory.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
+import { IoMdArrowRoundBack } from "react-icons/io";
 
 const AddSubCategory = () => {
   const [subCategory, setSubCategory] = useState({
@@ -41,42 +42,76 @@ const AddSubCategory = () => {
   };
 
   return (
-    <div>
-      <form action="" onSubmit={AddSubCategory}>
-        <label htmlFor="newSubcategory">Category name</label>
-        <input
-          type="text"
-          name="name"
-          placeholder="enter category name"
-          value={subCategory.name}
-          onChange={handleChange}
-        />
-        <label htmlFor="imgage">Peste imageUrl</label>
-        <input
-          type="text"
-          name="imageUrl"
-          placeholder="put your image url"
-          value={subCategory.imageUrl}
-          onChange={handleChange}
-        />
-        <label htmlFor="status">Status</label>
-        <input
-          type="text"
-          name="status"
-          placeholder="give status"
-          value={subCategory.status}
-          onChange={handleChange}
-        />
-        <label htmlFor="categoryId">CategoryId</label>
-        <input
-          type="text"
-          name="categoryId"
-          placeholder="Enter categoryId"
-          value={subCategory.categoryId}
-          onChange={handleChange}
-        />
-        <button className="bg-green-600 p-1 rounded-md">Add Category</button>
-      </form>
+    <div className="ml-72 mt-20 p-4 shadow-md rounded-md m-2 h-[85vh]">
+      <div className="flex items-center">
+        <Link to={"/subcategory"}>
+          <IoMdArrowRoundBack className="size-8" />
+        </Link>
+        <p className="text-2xl ml-5 font-semibold">Add SubCategory</p>
+      </div>
+      <div className="mt-2">
+        <form action="" onSubmit={AddSubCategory}>
+          <label
+            htmlFor="newSubcategory"
+            className="text-xl font-semibold text-gray-700 mb-2"
+          >
+            Category name
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="enter category name"
+            value={subCategory.name}
+            onChange={handleChange}
+            className="w-full px-4 mt-3 mb-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
+          />
+          <label
+            htmlFor="imgage"
+            className="text-xl font-semibold text-gray-700 mb-2"
+          >
+            Peste imageUrl
+          </label>
+          <input
+            type="text"
+            name="imageUrl"
+            placeholder="put your image url"
+            value={subCategory.imageUrl}
+            onChange={handleChange}
+            className="w-full px-4 mt-3 mb-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
+          />
+          <label
+            htmlFor="status"
+            className="text-xl font-semibold text-gray-700 mb-2"
+          >
+            Status
+          </label>
+          <input
+            type="text"
+            name="status"
+            placeholder="give status"
+            value={subCategory.status}
+            onChange={handleChange}
+            className="w-full px-4 mt-3 mb-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
+          />
+          <label
+            htmlFor="categoryId"
+            className="text-xl font-semibold text-gray-700 mb-2"
+          >
+            CategoryId
+          </label>
+          <input
+            type="text"
+            name="categoryId"
+            placeholder="Enter categoryId"
+            value={subCategory.categoryId}
+            onChange={handleChange}
+            className="w-full px-4 mt-3 mb-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
+          />
+          <button className="bg-[#662671] hover:bg-[#823c8f] text-white  p-2 rounded-md">
+            Add SubCategory
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
