b93a399a37b8de705935b2f5c68a6747311a6c08
maheshpawar3819 <pawarmahesh3819@gmail.com>
2024-11-12 12:57:28 +0530

    modify Product Component code

--- DigitalFlake/Client/DigitalFlake/src/Components/Product/Product.jsx
+++ DigitalFlake/Client/DigitalFlake/src/Components/Product/Product.jsx
@@ -1,5 +1,4 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import useGetProducts from "../Hooks/Product/useGetProducts";
 import { useSelector } from "react-redux";
