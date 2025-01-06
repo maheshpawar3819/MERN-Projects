b88a09dd9477e6962f8dd7de057977119457c508
maheshpawar3819 <pawarmahesh3819@gmail.com>
2025-01-06 13:29:46 +0530

    add dropdown insted of input field in Subcategory component

--- DigitalFlake/Client/DigitalFlake/src/Components/SubCategory/AddSubCategory.jsx
+++ DigitalFlake/Client/DigitalFlake/src/Components/SubCategory/AddSubCategory.jsx
@@ -3,8 +3,13 @@ import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { toast } from "react-toastify";
+import useGetCategory from "../Hooks/Category/useGetCategory";
+import { useSelector } from "react-redux";
 
 const AddSubCategory = () => {
+  useGetCategory();
+  const Category = useSelector((store) => store?.category?.category);
+  console.log(Category);
   const [subCategory, setSubCategory] = useState({
     name: "",
     imageUrl: "",
@@ -106,14 +111,25 @@ const AddSubCategory = () => {
           >
             CategoryId
           </label>
-          <input
-            type="text"
+          <select
             name="categoryId"
-            placeholder="Enter the parent Category ID (e.g., 1)"
             value={subCategory.categoryId}
             onChange={handleChange}
             className="w-full px-4 mt-3 mb-3 py-2 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-blue-500 focus:outline-none focus:border-blue-500 transition duration-300 ease-in-out"
-          />
+          >
+            <option value="" disabled className="text-gray-400">
+              Select a Category
+            </option>
+            {Category.map((category) => (
+              <option
+                key={category.id}
+                value={category.id}
+                className="text-gray-700 bg-white hover:bg-blue-100"
+              >
+                {category.name}
+              </option>
+            ))}
+          </select>
           <button className="bg-[#662671] hover:bg-[#823c8f] text-white  p-2 rounded-md">
             Add SubCategory
           </button>
